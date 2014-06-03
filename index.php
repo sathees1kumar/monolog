8845aa959df7441d384611c46096a520bde25548
Sathees <sathees@sathees-Vostro-2520.(none)>
2014-06-03 12:08:14 +0530

    Production Logic

--- index.php
+++ index.php
@@ -6,30 +6,39 @@ require_once('boostrap.php');
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Monolog\Formatter\JsonFormatter;
-use Monolog\Handler\FirePHPHandler;
-use Monolog\Handler\ChromePHPHandler;
+use Monolog\Formatter\HtmlFormatter;
+use Monolog\Handler\NativeMailerHandler;
 
 // create a log channel
 $log = new Logger('app');
 
 //Instantiate the handlers
 $streamHandler = new StreamHandler(__DIR__ . '/logs/error.log', Logger::NOTICE, false);
-$fireHandler = new FirePHPHandler();
-$chromeHandler = new ChromePHPHandler();
+$mailHandler = new NativeMailerHandler(
+                        'sathees1kumar@gmail.com', 
+                        'test',
+                        'noreply@gmail.com',
+                        Logger::ERROR,
+                        true
+                    );
+
 
 
 //Instantiate Formatters
 $jsonFormatter = new JsonFormatter();
+$htmlFormatter = new HtmlFormatter();
 
 //Set the formtter to handler
 $streamHandler->setFormatter($jsonFormatter);
+$mailHandler->setFormatter($htmlFormatter);
+
+//Set the contentType of mailer data
+$mailHandler->setContentType('text/html');
 
 
 //Push a stack of handlers
 $log->pushHandler($streamHandler);
-$log->pushHandler($fireHandler);
-$log->pushHandler($chromeHandler);
-
+$log->pushHandler($mailHandler);
 
 // add records to the log
 $log->addNotice('test messsage');
